72a0936e6e7e199691431829e8c596c9102d87bc
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-06-09 00:57:29 +0530

    sidebar nav

---
+++ components/Box.tsx
@@ -0,0 +1,18 @@
+interface BoxProps {
+    children: React.ReactNode;
+    className?: string;
+
+}
+
+const Box: React.FC<BoxProps> = ({
+    children,
+    className
+}) => {
+    return ( 
+        <div>
+            {children}
+        </div>
+    );
+}
+
+export default Box;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,11 @@
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 
+import { HiHome } from "react-icons/hi";
+import {BiSearch} from "react-icons/bi";
+
+import Box from "./Box";
+
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -18,11 +23,13 @@ const Sidebar: React.FC<SidebarProps> = ({
 
     const routes = useMemo(() => [
         {
+            icon: HiHome,
             label:'Home',
             active: pathname !== '/search',
             href: '/',
         },
         {
+            icon: BiSearch,
             label: 'Search',
             active: pathname !== '/search',
             href: '/search',
@@ -31,8 +38,24 @@ const Sidebar: React.FC<SidebarProps> = ({
     ], []);
 
     return (
-        <div>
-            {children}
+        <div className="flex h-full">
+            <div
+              className="
+              hidden
+              md:flex
+              flex-col
+              gap-y-2
+              bg-black
+              h-full
+              w-[300px]
+              p-2
+              "
+            >
+                <Box>
+                    Sidebar Navigation
+                </Box>
+            </div>
+           
         </div>
     );
 }
